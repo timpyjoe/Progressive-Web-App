9650f696b1031310de12c2e15d319600ebdd24dd
Tim Poffenberger <timpoffenberger23@gmail.com>
2023-12-02 20:51:28 -0600

    Fixes database.js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,36 +18,35 @@ const initdb = async () =>
     },
   });
 
-/*
-  We need to add some code below which will take updated content and save it to IndexedDB.
-*/
+
 export const putDb = async (content) => {
-  // First, create a variable, and set it to asyncronously await the opening of the database. Replace the items in all caps
+
   
-  // TODO: Change YOUR_OPEN_DB_VAR to whatever variable name you wanT. Note that you'll then need to change any other occcurences of YOUR_OPEN_DB_VAR to the same variable name.
-  const YOUR_OPEN_DB_VAR = await openDB(DB_NAME, 1);
+  const openedDB = await openDB(DB_NAME, 1);
 
-  // TODO: Now create a variable for the transaction; again, this will be referenced below.
-  const YOUR_TX_VAR = YOUR_OPEN_DB_VAR.transaction(DB_NAME, 'readwrite');
+  const dbTransaction = openedDB.transaction(DB_NAME, 'readwrite');
 
-  // TODO: Now create a variable for the store
-  const YOUR_STORE_VAR = YOUR_TX_VAR.objectStore(DB_NAME);
+  const myStore = dbTransaction.objectStore(DB_NAME);
 
-  const request = YOUR_STORE_VAR.put({ id: 1, value: content });
+  const request = myStore.put({ id: 1, value: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result.value);
 };
 
-/*
-  We need to add some code below which will get all content from IndexedDB.
-*/
+
 export const getDb = async () => {
-  // You can duplicate the same lines of code from above, except that the transaction will be 'readonly'
   
-  // TODO: Copy LINES 28, 31 and 34 above; the new line 31 code should be "readonly"
+  const openedDB = await openDB(DB_NAME, 1);
+
+  
+  const dbTransaction = openedDB.transaction(DB_NAME, 'readonly');
+
+  
+  const myStore = dbTransaction.objectStore(DB_NAME);
+
 
   // Leave the rest as-is
-  const request = store.get(1);
+  const request = myStore.get(1);
   const result = await request;
   result
     ? console.log('🚀 - data retrieved from the database', result.value)
